5826a737057b82f11ef85bf5bbb8492ab0327c23
Yeonwook (Eric) Kim <77266996+x4kUHD@users.noreply.github.com>
2023-09-23 01:36:22 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,2 +1,47 @@
-import Swup from "swup";
-const swup = new Swup();
+// const emailButton = document.getElementById("email");
+// emailButton.addEventListener("click", copyEmail);
+
+// function copyEmail() {
+//   const email = "yeonwookerickim@gmail.com";
+
+//   // Create a temporary input element to copy the email text
+//   const tempInput = document.createElement("input");
+//   tempInput.value = email;
+//   document.body.appendChild(tempInput);
+
+//   // Select and copy the email text
+//   tempInput.select();
+//   Document.execCommand("copy");
+
+//   // Remove the temporary input element
+//   document.body.removeChild(tempInput);
+
+//   // Provide some feedback to the user (optional)
+//   alert("Email address copied to clipboard: " + email);
+// }
+
+// JavaScript code to handle the "email" button click
+document.addEventListener("DOMContentLoaded", function () {
+  const emailButton = document.getElementById("email");
+  emailButton.addEventListener("click", copyEmail);
+
+  function copyEmail() {
+    // Get the email address to copy
+    const email = "yeonwookerickim@gmail.com"; // Replace with your actual email address
+
+    // Create a temporary input element to copy the email text
+    const tempInput = document.createElement("input");
+    tempInput.value = email;
+    document.body.appendChild(tempInput);
+
+    // Select and copy the email text
+    tempInput.select();
+    document.execCommand("copy");
+
+    // Remove the temporary input element
+    document.body.removeChild(tempInput);
+
+    // Provide some feedback to the user (optional)
+    alert("Email address copied to clipboard: " + email);
+  }
+});
